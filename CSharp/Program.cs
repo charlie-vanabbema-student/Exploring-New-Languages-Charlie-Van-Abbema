004ff1cca838a7e3bd6d0a720e20da68d94aa9b7
charlie-vanabbema-student <112420966+charlie-vanabbema-student@users.noreply.github.com>
2023-02-06 19:45:20 +0000

    so far

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,7 +1,33 @@
-﻿public class Program
+﻿using System;
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World");
+        string text = File.ReadAllText(@"../secretMessage.txt");
+        Console.WriteLine(AverageSecretMessage(text));
+        static float AverageSecretMessage(string text)
+        {
+            string[] numberstext = text.Split(" ");
+            int[] numbers = Array.ConvertAll(numberstext, s => int.Parse(s));
+            int sum = 0;
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return (float)sum / numbers.Length;
+        }
+
+        static string DecodeSecretMessage(int[] arr)
+        {
+            string secretmessage = "";
+            foreach (char value in arr)
+            {
+                secretmessage += Convert.ToChar(value);
+            }
+            return secretmessage;
+        }
+        
+        
     }
 }
