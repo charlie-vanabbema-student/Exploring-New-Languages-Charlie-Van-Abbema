63614eb359b1c7094d5d09d89141d5fca3e8cd6d
charlie-vanabbema-student <112420966+charlie-vanabbema-student@users.noreply.github.com>
2023-02-27 19:42:35 +0000

    So farrrrrrrrrrrrrrrr

--- Go/ExploringLanguages.go
+++ Go/ExploringLanguages.go
@@ -2,32 +2,84 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"io/ioutil"
-	"log"
-	"math/rand"
+	"strconv"
 	"strings"
-	"time"
-	"github.com/google/uuid"
+
+	// "container/list"
+
+	// "io"
+	"os"
+	// "path/filepath"
+	// "io/ioutil"
+	// "log"
+	// "math/rand"
+	// "time"
+	// "github.com/google/uuid"
 )
 
-func ReadFile(filepath string) []string {
-	b, err := ioutil.ReadFile(filePath) // read words from file
+func main() {
+	b, err := os.ReadFile("../secretMessage.txt")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Print(err)
 	}
 
-	str := string(b) // convert content to a 'string'
-	words := strings.Split(str, "\n")
-	return words
-}
-func AverageSecretMessage() {
-	// numbersText := ReadSecretMessage().Split(" ")
+	var str string = string(b)
+
+	// fmt.Print(str)
+	println(AverageSecretMessage(str))
+	println(DecodeSecretMessage(str))
+	println(FizzBuzz(15))
 }
 
-func main() {
-	ReadFile("../secretMessage.txt")
-	// AverageSecretMessage()
+func AverageSecretMessage(str string) int {
+	numberstext := strings.Fields(str)
 
+	var numbers = []int{}
+
+	for _, i := range numberstext {
+		num, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, num)
+	}
+	sum := 0
+	for i := 0; i < len(numbers); i++ {
+		sum += numbers[i]
+	}
+	return sum / len(numbers)
+}
+
+func DecodeSecretMessage(str string) string {
+	numberstext := strings.Fields(str)
+	secretmessage := ""
+	var numbers = []int{}
+
+	for _, i := range numberstext {
+		num, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, num)
+	}
+	for i := 0; i < len(numbers); i++ {
+		secretmessage += string(rune(numbers[i]))
+	}
+	return secretmessage
+}
+
+func FizzBuzz(n int) []string {
+	arr := []string{}
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			arr = append(arr, "FizzBuzz")
+		} else if i%3 == 0 {
+			arr = append(arr, "Fizz")
+		} else if i%5 == 0 {
+			arr = append(arr, "Buzz")
+		} else {
+			arr = append(arr, strconv.Itoa(i))
+		}
+	}
+	return arr
 }
